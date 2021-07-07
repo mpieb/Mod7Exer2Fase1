f6438263da9ee5c4cbb6d02046604cabf841c5ce
Miquel <76565907+mpieb@users.noreply.github.com>
2021-07-07 19:13:43 +0200

    Add files via upload

---
+++ src/Mod7Exer2Fase1/Camio.java
@@ -0,0 +1,10 @@
+package Mod7Exer2Fase1;
+
+class Camio extends Vehicle {
+    public Camio(String plateNombre, String plateChar, String brand, String color) {
+            super(plateNombre, plateChar, brand, color);
+    }
+    public Camio(String brandRodes, double diametre){
+		super(brandRodes, diametre);
+    }
+}
